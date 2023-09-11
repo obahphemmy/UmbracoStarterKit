232c45f938e09172fc4d17fd496f02bcd97a5850
Obafemi <obafemi.bankole@senior.co.uk>
2023-09-11 14:48:56 +0100

    Add project files.

---
+++ SampleStarterKit.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace SampleStarterKit.Core.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static int GetContentId(this IPublishedContent content)
+        {
+            return content.Id;
+        }
+    }
+}
---
+++ SampleStarterKit/Migrations/StarterKitPackageMigrationPlan.cs
@@ -0,0 +1,20 @@
+﻿using SampleStarterKit.Migrations;
+using System;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace SampleStarterKit.Migrations
+{
+    public class StarterKitPackageMigrationPlan : PackageMigrationPlan
+    {
+        public StarterKitPackageMigrationPlan()
+            : base("SampleStarterKit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("087C5260-895E-4D64-8FA9-D24942769140"));
+        }
+    }
+}
